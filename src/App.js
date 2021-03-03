02a277c54dc5e9e2f7da5658950efcacdf30954e
toby <krisella74@gmail.com>
2021-03-03 03:43:11 +0100

    up and running

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Setup from "./Setup";
+import Runner from "./Runner";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [word, setWord] = useState("");
+  const [wpm, setWpm] = useState(10);
+
+  const [startRunner, setStartRunner] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Words per minute.</h3>
+
+      <Setup
+        word={word}
+        setWord={setWord}
+        wpm={wpm}
+        setWpm={setWpm}
+        onStart={() => setStartRunner(true)}
+      />
+      <Runner />
     </div>
   );
 }
